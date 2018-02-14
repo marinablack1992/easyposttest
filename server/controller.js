d64da6de4f754d5b3d266654de5cd12bf87c12ac
Marina <marinablack1992@gmail.com>
2018-02-13 16:45:04 -0800

    added some end points, starting to work on front end to have endpoints fully tested.

--- server/controller.js
+++ server/controller.js
@@ -29,16 +29,17 @@ module.exports = {
         });
 
         verifiableAddress.save().then(address => {
-            !address.verifications.delivery.success ? console.log('address failed to verify. error: ', address.verifications.delivery.errors) : console.log('success! verifications: ', address.verifications)
+            !address.verifications.delivery.success ? console.log('address failed to verify. error: ', address.verifications.delivery.errors) : console.log('success! verifications: ', address.verifications);
             res.status(200).send(address);
-        })
+        });
     },
     retrieveAddress: (req, res, next) => {
-        const { addressID } = req.params
+        const { addressID } = req.params;
+
         api.Address.retrieve(addressID).then(data => {
             console.log(data)
             res.status(200).send(data)
-        })
+        });
     },
     createParcel: (req, res, next) => {
         const parcel = new api.Parcel({
@@ -53,9 +54,42 @@ module.exports = {
         });
     },
     retrieveParcel: (req, res, next) => {
-        const { parcelID } = req.params
+        const { parcelID } = req.params;
+
         api.Parcel.retrieve(parcelID).then(data => {
             res.status(200).send(data)
+        });
+    },
+    createShipment: (req, res, next) => {
+        const { toAddress, fromAddress, parcel, customsInfo } = req.body;
+        // to make this work on the front end, capture toAddress, fromAddress, parcel & customsInfo objects from responses to state that way they can be sent in on the body to complete this request.
+
+        const shipment = new api.Shipment({
+            to_address: toAddress,
+            from_address: fromAddress,
+            parcel: parcel,
+            customs_info: customsInfo
+        });
+
+        shipment.save().then(data => {
+            res.status(200).send(data)
+        });
+    },
+    buyShipment: (req, res, next) => {
+        const { shipmentID } = req.params;
+
+        api.Shipment.retrieve(shipmentID).then(shipment => {
+            shipment.buy(s.lowestRate(), 250.00).then(data => {
+                console.log(data);
+            })
+        });
+    },
+    labelExample: (req, res, next) => {
+        const { shipmentID } = req.params;
+        api.Shipment.retrieve(shipmentID).then(shipment => {
+            shipment.convertLabelFormat('PDF').then(pdf => {
+                res.status(200).send(pdf)
+            })
         });
     },
 }
--- server/server.js
+++ server/server.js
@@ -15,10 +15,13 @@ const apiKey = process.env.API_TEST_KEY;
 const api = new EasyPost(apiKey);
 
 app.get(`/api/get-address/:addressID`, ctrl.retrieveAddress);
-app.get(`/api/get-parcel/:parcelID`, ctrl.retrieveParcel)
+app.get(`/api/get-parcel/:parcelID`, ctrl.retrieveParcel);
+app.get('/api/label-example/:shipmentID', ctrl.labelExample)
 
 app.post('/api/create-shipment', ctrl.createAddress);
 app.post('/api/create-parcel', ctrl.createParcel);
+app.post('/api/create-shipment', ctrl.createShipment);
+app.post('/api/buy-shipment/:shipmentID', ctrl.buyShipment);
 
 
 const PORT = 3001;
